96aa23d5d4f75de90b12ceb3ff5baf25d948a00b
Warrick Smith <warrick.smith@neo.nz>
2023-08-15 21:56:41 +1200

    chore: add images

--- src/components/box4/Box4Content.tsx
+++ src/components/box4/Box4Content.tsx
@@ -1,42 +1,11 @@
 import styled from 'styled-components'
 
-const images = [
-'/image1.jpg',
-'/image2.jpg',
-'/image3.jpg'
-];
-
-const Grid = styled.div`
-display: flex;
-
-img {
-width: 33.33%;
-cursor: pointer;
-}
-
-@media (max-width: 768px) {
-flex-direction: column;
-
-cssimg {
-  width: 100%;
-}
-}
-`;
-
-interface GridComponentProps {
-images: string[];
-}
-
-export const GridComponent = ({ images }: GridComponentProps) => (
-{images.map(img => (product))}
-);
-
 const Content = styled.div`
   color: var(--color-alt);
   font-size: var(--fs-lge);
 `
 const Box4Content = () => {
-return <Content><Grid><GridComponent images={images} /></Grid>  </Content>
+return <Content>Box4 Content</Content>
 }
 
 export default Box4Content
