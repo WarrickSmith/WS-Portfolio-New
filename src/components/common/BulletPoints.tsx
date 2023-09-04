86d98e0557077d840a08e21b86fe56e259b0715d
Warrick Smith <warrick.smith@neo.nz>
2023-09-04 19:26:38 +1200

    fix: update bulletpoint formatting

--- src/components/common/BulletPoints.tsx
+++ src/components/common/BulletPoints.tsx
@@ -7,25 +7,28 @@ interface Props {
   image: string
 }
 
-const Container = styled(motion.div)`
+const Container = styled(motion.a)`
   position: relative;
   cursor: pointer;
   border-radius: 0.5rem;
-  background-size: 100% 100%;
+  background-size: contain;
+  background-repeat: no-repeat;
   background-position: center;
+  height: 100%;
 `
 
 const TextContainer = styled(motion.div)`
   position: relative;
-  background-color: orange;
-  /* top: 0; */
-  /* left: 0; */
-  /* width: 100%; */
-  /* height: 100%; */
+  background-color: var(--color-alt);
+  height: 100%;
 `
 
 const Title = styled.h1`
   color: white;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Bullets = styled.ul`
@@ -36,27 +39,27 @@ const BulletPoint = styled.li``
 
 const BulletPoints: React.FC<Props> = ({ title, points, image }) => {
   return (
-    <Container
-      initial={{ backgroundImage: `url(${image})` }}
-      whileHover={{
-        backgroundImage: 'none',
-        transition: { duration: 1 },
-      }}
-      animate={{ transition: { duration: 1 } }}
-    >
-      <TextContainer
-        initial={{ opacity: 0 }}
-        whileHover={{ opacity: 1, transition: { duration: 1 } }}
+      <Container
+        initial={{ backgroundImage: `url(${image})` }}
+        whileHover={{
+          backgroundImage: 'none',
+          transition: { duration: 1 },
+        }}
         animate={{ transition: { duration: 1 } }}
       >
-        <Title>{title}</Title>
-        <Bullets>
-          {points.map((point, index) => (
-            <BulletPoint key={index}>{point}</BulletPoint>
-          ))}
-        </Bullets>
-      </TextContainer>
-    </Container>
+        <TextContainer
+          initial={{ opacity: 0 }}
+          whileHover={{ opacity: 1, transition: { duration: 1 } }}
+          animate={{ transition: { duration: 1 } }}
+        >
+          <Title>{title}</Title>
+          <Bullets>
+            {points.map((point, index) => (
+              <BulletPoint key={index}>{point}</BulletPoint>
+            ))}
+          </Bullets>
+        </TextContainer>
+      </Container>
   )
 }
 
