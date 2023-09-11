e5753ffff3a46fa3bd62faabd50791f32076738f
Warrick Smith <warrick.smith@neo.nz>
2023-09-11 13:46:45 +1200

    feat: add cell2 personal info component

--- src/components/box3/cellContent/Cell2.tsx
+++ src/components/box3/cellContent/Cell2.tsx
@@ -1,7 +1,41 @@
 import styled from 'styled-components'
 
-const Cell2 = () => {
-  return <div>Cell 2</div>
+const StyledInfo = styled.div`
+  margin-bottom: 1rem;
+`
+
+const Label = styled.span`
+  font-weight: 500;
+  color: var(--color-alt);
+`
+
+const Value = styled.span`
+  margin-left: 1rem;
+  font-weight: 400;
+`
+
+interface InfoItemProps {
+  label: string
+  value: string
 }
+
+const InfoItem: React.FC<InfoItemProps> = ({ label, value }) => (
+  <StyledInfo>
+    <Label>{label}</Label>
+    <Value>{value}</Value>
+  </StyledInfo>
+)
+
+const Cell2 = () => (
+  <div>
+    <InfoItem label="First Name:" value="Warrick" />
+    <InfoItem label="Last Name:" value="Smith" />
+    <InfoItem label="Nationality:" value="New Zealand" />
+    <InfoItem label="Employment Status:" value="Available" />
+    <InfoItem label="Phone:" value="+64 21 0248 8139" />
+    <InfoItem label="Address:" value="Auckland, New Zealand" />
+    <InfoItem label="Spoken Languages:" value="English" />
+  </div>
+)
 
 export default Cell2
