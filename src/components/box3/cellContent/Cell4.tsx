872acf22860efff67c786a457fa569df1621c9fd
Warrick Smith <warrick.smith@neo.nz>
2023-09-19 15:08:09 +1200

    refactor: render using map

--- src/components/box3/cellContent/Cell4.tsx
+++ src/components/box3/cellContent/Cell4.tsx
@@ -29,109 +29,47 @@ const Component = styled(motion.div)<ComponentProps>`
 `
 
 const Cell4: React.FC<PageDisplayProps> = ({ selectedOption }) => {
-  const trackCard = {
-    offset: 0,
-    zIndex: 3,
+  const components = [
+    { key: 'experience', zIndex: 3, offset: 0 },
+    { key: 'education', zIndex: 2, offset: 10 },
+    { key: 'skills', zIndex: 1, offset: 20 },
+  ]
+
+  const selectedComponent = components.find(
+    (component) => component.key === selectedOption
+  )
+  const zIndex3Component = components.find(
+    (component) => component.zIndex === 3
+  )
+
+  if (selectedComponent && zIndex3Component) {
+    const tempZIndex = selectedComponent.zIndex
+    const tempOffset = selectedComponent.offset
+
+    selectedComponent.zIndex = zIndex3Component.zIndex
+    selectedComponent.offset = zIndex3Component.offset
+
+    zIndex3Component.zIndex = tempZIndex
+    zIndex3Component.offset = tempOffset
   }
-
-  trackCard.offset =
-    selectedOption === 'experience'
-      ? 0
-      : selectedOption === 'education'
-      ? 10
-      : selectedOption === 'skills'
-      ? 20
-      : 0
-
-  trackCard.zIndex =
-    selectedOption === 'experience'
-      ? 3
-      : selectedOption === 'education'
-      ? 2
-      : selectedOption === 'skills'
-      ? 1
-      : 3
 
   return (
     <Container>
       <AnimatePresence>
-        {selectedOption === 'experience' ? (
+        {components.map((component) => (
           <Component
-            key="experience"
-            initial={{ x: 0, y: 0 }}
+            key={component.key}
             animate={{ x: 0, y: 0 }}
             exit={{}}
             transition={{ duration: 0.5 }}
-            zIndex={3}
-            offset={0}
+            zIndex={component.zIndex}
+            offset={component.offset}
           >
-            <Experience />
+            {component.key === 'experience' && <Experience />}
+            {component.key === 'education' && <Education />}
+            {component.key === 'skills' && <Skills />}
           </Component>
-        ) : (
-          <Component
-            key="noexperience"
-            initial={{ x: trackCard.offset, y: trackCard.offset }}
-            animate={{ x: trackCard.offset, y: trackCard.offset }}
-            exit={{}}
-            transition={{ duration: 0.5 }}
-            zIndex={trackCard.zIndex}
-            offset={trackCard.offset}
-          >
-            <Experience />
-          </Component>
-        )}
-
-        {selectedOption === 'education' ? (
-          <Component
-            key="education"
-            initial={{ x: 0, y: 0 }}
-            animate={{ x: 0, y: 0 }}
-            exit={{}}
-            transition={{ duration: 0.5 }}
-            zIndex={3}
-            offset={0}
-          >
-            <Education />
-          </Component>
-        ) : (
-          <Component
-            key="noeducation"
-            initial={{ x: 10, y: 10 }}
-            animate={{ x: 10, y: 10 }}
-            exit={{}}
-            transition={{ duration: 0.5 }}
-            zIndex={2}
-            offset={10}
-          >
-            <Experience />
-          </Component>
-        )}
-
-        {selectedOption === 'skills' ? (
-          <Component
-            key="skills"
-            initial={{ x: 0, y: 0 }}
-            animate={{ x: 0, y: 0 }}
-            exit={{}}
-            transition={{ duration: 0.5 }}
-            zIndex={3}
-            offset={0}
-          >
-            <Skills />
-          </Component>
-        ) : (
-          <Component
-            key="noskills"
-            initial={{ x: 20, y: 20 }}
-            animate={{ x: 20, y: 20 }}
-            exit={{}}
-            transition={{ duration: 0.5 }}
-            zIndex={1}
-            offset={20}
-          >
-            <Experience />
-          </Component>
-        )}
+        ))}
       </AnimatePresence>
     </Container>
   )
