4ecc53d985c7ef8554f9cd8b9445e8b8f5ae562c
Warrick Smith <warrick.smith@neo.nz>
2023-09-22 13:53:28 +1200

    fix: update how mainContainer handles mobile view with browser frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import {useEffect, useState} from 'react'
 import { MainContainer } from './components/common/GridComponents'
 import { MainPage } from './components/MainPage'
 
 const App = () => {
+  const [containerHeight, setContainerHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setContainerHeight(window.innerHeight)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
-    <MainContainer>
+    <MainContainer height={containerHeight}>
       <MainPage />
     </MainContainer>
   )
