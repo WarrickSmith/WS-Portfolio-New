44986de4ed476b28e5a387e2e2a04a37f03337f3
Warrick Smith <warrick.smith@neo.nz>
2024-03-13 09:19:01 +1300

    fix: update vite build chunksize limit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000,    
+  },
   base: '/',
   plugins: [react()],
   preview: {
