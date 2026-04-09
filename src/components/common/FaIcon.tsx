f02a13c1f3468098954d8b9acd4f1f565027964d
Warrick Smith <warrick@baybox.co.nz>
2026-04-09 17:04:22 +1200

    refactor: tree-shake FontAwesome icon imports

--- src/components/common/FaIcon.tsx
+++ src/components/common/FaIcon.tsx
@@ -1,41 +1,47 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition } from '@fortawesome/fontawesome-common-types'
-import { IconName as SolidIconName } from '@fortawesome/free-solid-svg-icons'
-import { IconName as RegularIconName } from '@fortawesome/free-regular-svg-icons'
-import { IconName as BrandIconName } from '@fortawesome/free-brands-svg-icons'
-import * as solidIcons from '@fortawesome/free-solid-svg-icons'
-import * as brandIcons from '@fortawesome/free-brands-svg-icons'
-import * as regularIcons from '@fortawesome/free-regular-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import {
+  faArrowUpRightFromSquare,
+  faBriefcase,
+  faCalendar,
+  faCode,
+  faComments,
+  faCompass,
+  faEnvelope,
+  faGraduationCap,
+  faLocationDot,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'
 
-type IconMap = {
-  [key in SolidIconName | BrandIconName | RegularIconName]: IconDefinition
+const iconMap: Record<string, IconDefinition> = {
+  faArrowUpRightFromSquare,
+  faBriefcase,
+  faCalendar,
+  faCode,
+  faComments,
+  faCompass,
+  faEnvelope,
+  faGithub,
+  faGraduationCap,
+  faLocationDot,
+  faUser,
 }
-
-const icons = solidIcons as unknown as IconMap
-const brandIconsMap = brandIcons as unknown as IconMap
-const regularIconsMap = regularIcons as unknown as IconMap
 
 type FaIconProps = {
   icon: string
   className?: string
-  type?: 'solid' | 'regular'
 }
 
-const FaIcon = ({ icon, className, type = 'solid' }: FaIconProps) => {
-  let IconComponent: IconDefinition | null = null
+const FaIcon = ({ icon, className }: FaIconProps) => {
+  const iconDefinition = iconMap[icon]
 
-  if (type === 'solid' && icons[icon as SolidIconName]) {
-    IconComponent = icons[icon as SolidIconName]
-  } else if (type === 'regular' && regularIconsMap[icon as RegularIconName]) {
-    IconComponent = regularIconsMap[icon as RegularIconName]
-  } else if (brandIconsMap[icon as BrandIconName]) {
-    IconComponent = brandIconsMap[icon as BrandIconName]
-  } else {
+  if (!iconDefinition) {
     // Handle error if icon is not found
     return <div>Icon not found</div>
   }
 
-  return <FontAwesomeIcon icon={IconComponent} className={className} />
+  return <FontAwesomeIcon icon={iconDefinition} className={className} />
 }
 
 export default FaIcon
